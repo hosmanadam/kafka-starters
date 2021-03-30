e05b695bcedb3f783b91f9e15ceb16c3b30cb9ee
Adam Hosman <Adam_Hosman@epam.com>
2021-03-31 01:17:01 +0200

    Refactor

---
+++ src/main/java/com/adamhosman/kafkaplayground/Constants.java
@@ -0,0 +1,9 @@
+package com.adamhosman.kafkaplayground;
+
+public class Constants {
+
+    public static final String BOOTSTRAP_SERVER_IP = "localhost:9092";
+    public static final String TOPIC_1 = "topic_1";
+    public static final String GROUP_1 = "group_1";
+
+}
--- src/main/java/com/adamhosman/kafkaplayground/Producer.java
+++ src/main/java/com/adamhosman/kafkaplayground/Producer.java
@@ -10,19 +10,20 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
+import static com.adamhosman.kafkaplayground.Constants.BOOTSTRAP_SERVER_IP;
+import static com.adamhosman.kafkaplayground.Constants.TOPIC_1;
 import static org.apache.kafka.clients.producer.ProducerConfig.*;
 
 public class Producer {
 
-    public static final String TOPIC_1 = "topic_1";
     public static final Callback CALLBACK = createCallback();
     public static final Logger log = LoggerFactory.getLogger(Producer.class);
 
     public static void main(String[] args) {
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(createProducerProperties());
+        KafkaProducer<String, String> producer = new KafkaProducer<>(createProducerProperties());
         for (int i = 1; i < 20; i++) {
             producer.send(
-                    new ProducerRecord<String, String>(
+                    new ProducerRecord<>(
                             TOPIC_1,
                             Integer.toString(i % 2), // Send odd vs. even to different partitions
                             String.format("Sending message no. %s", i)
@@ -34,23 +35,21 @@ public class Producer {
     }
 
     private static Callback createCallback() {
-        return new Callback() {
-            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                if (e == null) {
-                    log.info(String.format("Topic: %s", recordMetadata.topic()));
-                    log.info(String.format("Partition: %s", recordMetadata.partition()));
-                    log.info(String.format("Offset: %s", recordMetadata.offset()));
-                    log.info(String.format("Timestamp: %s", recordMetadata.timestamp()));
-                } else {
-                    log.error("No luck :(", e);
-                }
+        return (recordMetadata, e) -> {
+            if (e == null) {
+                log.info(String.format("Topic: %s", recordMetadata.topic()));
+                log.info(String.format("Partition: %s", recordMetadata.partition()));
+                log.info(String.format("Offset: %s", recordMetadata.offset()));
+                log.info(String.format("Timestamp: %s", recordMetadata.timestamp()));
+            } else {
+                log.error("No luck :(", e);
             }
         };
     }
 
     private static Properties createProducerProperties() {
         Properties p = new Properties();
-        p.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        p.setProperty(BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER_IP);
         p.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         p.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return p;
